4db291598d3cff6c76c1679fa2bd766fdb4874c9
Tran Duc Ngoc <tranducngoc134@gmail.com>
2021-03-09 11:14:32 +0700

    add

--- src/components/Root/Counter/Counter.js
+++ src/components/Root/Counter/Counter.js
@@ -49,6 +49,7 @@ const Counter = () => {
         <Button onClick={() => dispatch(1)}>+1</Button>
         <Button onClick={() => dispatch(2)}>+2</Button>
         <Button onClick={() => dispatch(3)}>+3</Button>
+        <Button onClick={() => dispatch(4)}>-4</Button>
       </ButtonRow>
     </div>
   );
